1bdf6049c470fbf3bc6a18377a65d553936fad50
Joao Brunet <joaoarthurbm@gmail.com>
2015-12-09 13:29:04 -0300

    handling errors

--- scripts/cifra_parser.py
+++ scripts/cifra_parser.py
@@ -1,11 +1,18 @@
-#zeze-di-camargo-e-luciano-voce-vai-ver.html
+#coding: utf-8
 from bs4 import BeautifulSoup
 import sys
+if len(sys.argv) != 2:
+    print 'usage: python cifra_parser.py cifra.html'
+    sys.exit()
+
 soup = BeautifulSoup(open(sys.argv[1]), "html.parser")
 try:
-    print 'Tom %s' % soup.find(id="cifra_tom").find('a').text
+    out = sys.argv[1]
+    out += ',%s,' % soup.find(id="cifra_tom").find('a').text.encode('utf-8')
     notas = soup.find(id="cifra_tom").parent.find_all('b')
     for nota in notas:
-        print nota.text, 
-except:
-    print sys.argv[1]
+        out += '%s;' % nota.text.encode('utf-8') 
+    print out[0:-1]
+except Exception as e:
+    print 'ERRO %s' % sys.argv[1]
+    print e

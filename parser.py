a42aa00370bf173fdf4a13dd5a0b1ed296d84c8c
Joao Brunet <joaoarthurbm@gmail.com>
2015-12-09 23:20:49 -0300

    moving

--- parser.py
+++
@@ -1,6 +0,0 @@
-import sys
-start = 'www.cifraclub.com.br'
-for linha in open(sys.argv[1]):
-    if '<a href=' in linha and len(linha) < 200:
-        tokens = linha.split('/')
-        print start+'/'+tokens[1]

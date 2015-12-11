22b354f2e07783817af1ece0d7e0f93dd2bde284
Fabio Morais <fabio@tricolor.lsd.ufcg.edu.br>
2015-12-11 17:11:10 -0300

    numero de acessos das cifras

--- scripts/download_cifras.sh
+++ scripts/download_cifras.sh
@@ -1,9 +1,9 @@
-mkdir cifras
+mkdir cifras_erro
 while read line; do
     dir=`echo $line | awk -F'/' '{print $4}'`
     song=`echo $line | awk -F'/' '{print $5}'`
-    if [ ! -d "cifras/$dir" ]; then
-      mkdir cifras/$dir
+    if [ ! -d "cifras_erro/$dir" ]; then
+      mkdir cifras_erro/$dir
     fi
-    curl $line > cifras/$dir/$dir_$song.html
+    curl $line > cifras_erro/$dir/$dir_$song.html
 done < $1

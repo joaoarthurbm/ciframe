daee2be06f6acc58b2ea6bd9ae6a18504fbd1c7a
fabiomorais <fabio.jorge@gmail.com>
2015-12-12 12:58:43 -0300

    Correção do numero de visualizações e início da análise de popularidade

--- data/top/merge_datasets.R
+++ data/top/merge_datasets.R
@@ -3,7 +3,7 @@ library(dplyr)
 df_dataset = read.csv(file = "dataset.csv", header = F)
 colnames(df_dataset) = c("ARTISTA", "MUSICA", "TOM", "CIFRA")
 
-df_visu = read.csv(file = "visualizacoes.csv", header = F)
+df_visu = read.csv(file = "visualizacoes_pontos.csv", header = F)
 colnames(df_visu) = c("ARTISTA", "MUSICA", "VISUALIZACOES")
 
 colnames(df_visu)

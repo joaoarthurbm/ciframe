f769feff3fc67f2b46077279fd881243f1968d22
Clenimar <clenimar.filemon@gmail.com>
2015-12-13 10:08:27 -0300

    Site na internet

--- scripts/collect_song_list.sh
+++
@@ -1,6 +0,0 @@
-if [[ $# -eq 0 ]] ; then
-    echo 'usage: ./collect_song_list.sh artists.txt'
-    exit 1
-fi
-
-java -cp jsoup-1.8.3.jar:. DownloadAll $1
--- scripts/download_cifras.sh
+++
@@ -1,9 +0,0 @@
-mkdir cifras_erro
-while read line; do
-    dir=`echo $line | awk -F'/' '{print $4}'`
-    song=`echo $line | awk -F'/' '{print $5}'`
-    if [ ! -d "cifras_erro/$dir" ]; then
-      mkdir cifras_erro/$dir
-    fi
-    curl $line > cifras_erro/$dir/$dir_$song.html
-done < $1

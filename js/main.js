3925dbd1a64d7fc2a3fbacbce5fa609dff2e694e
Clenimar <clenimar.filemon@gmail.com>
2015-12-13 19:31:20 -0300

    Sequências famosas

--- js/main.js
+++ js/main.js
@@ -12,10 +12,12 @@
 
           $scope.musicas = [];
           $scope.meusAcordes = [];
+          $scope.seqFamosa = [];
 
           $scope.acordesMaiores = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
           $scope.acordesMenores = ["Cm", "C#m", "Dm", "D#m", "Em", "Fm", "F#m", "Gm", "G#m", "Am", "A#m", "Bm"];
           $scope.acordesComSetima = ["C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7"];
+          $scope.sequenciasFamosas = ['Bm - G - D - A', "C - G - Am - F", "Em - G", "C - A7 - Dm - G7", "Gm - F", "C - C7 - F - Fm"]
 
           $scope.tiraEBota = function(s) {
             if ($scope.meusAcordes.indexOf(s) == -1) {
@@ -46,9 +48,27 @@
               });
           }
 
+          $scope.pesquisarPorSequencia = function(i) {
+            $http.get("http://localhost:5000/porSequencia?sequencia=" + i +
+                  "&filtro-generos=" + JSON.stringify($scope.filtroEstilo) +
+                  "&filtro-artistas=" + JSON.stringify($scope.filtroArtista) +
+                  "&min=" + $scope.filtroFacilidade[0] +
+                  "&max=" + $scope.filtroFacilidade[1]
+                )
+                  .success(function (data, headers) {
+                    console.log(data);
+                    $scope.musicas = data;
+                  });
+          }
+
           $scope.pesquisarPorAcordes = function() {
+
+            function replaceAll(str, find, replace) {
+              return str.replace(new RegExp(find, 'g'), replace);
+            }
+
             $http.get(
-              "http://localhost:5000/rankByAcordes?acordes=" + JSON.stringify($scope.meusAcordes).replace('#', "%23") +
+              "http://localhost:5000/rankByAcordes?acordes=" + replaceAll(JSON.stringify($scope.meusAcordes), '#', "%23") +
                 "&filtro-generos=" + JSON.stringify($scope.filtroEstilo) +
                 "&filtro-artistas=" + JSON.stringify($scope.filtroArtista) +
                 "&min=" + $scope.filtroFacilidade[0] +

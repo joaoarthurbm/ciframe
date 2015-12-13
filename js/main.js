8cbfdb89a3e0f4c5fd3325c3a37e6c35d64d187d
Clenimar <clenimar.filemon@gmail.com>
2015-12-13 15:55:37 -0300

    Angular: pesquisa por acorde e por nome de música

---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('DecifraMe')
+        .controller('MainCtrl', function($scope, $http, $window) {
+
+          $scope.filtroArtista = ["Molejo"];
+          $scope.filtroEstilo = ["MPB"];
+          $scope.filtroFacilidade = [70, 100];
+          $scope.tituloMusica = "Vou voltar pra sacanagem";
+
+          $scope.musicas = [];
+          $scope.meusAcordes = [];
+
+          $scope.pesquisarPorNome = function() {
+            $http.get("http://localhost:5000/busca?musica=" + $scope.tituloMusica)
+              .success(function(data, headers) {
+                musicas = data;
+              })
+              .error(function(data, headers) {
+                console.log("Erro ao tentar pesquisar por nome!");
+              });
+          }
+
+          $scope.pesquisarPorAcordes = function() {
+            $http.get(
+              "http://localhost:5000/rankByAcordes?acordes=" + $scope.meusAcordes +
+                "&filtro-generos=" + $scope.filtroEstilo +
+                "&filtro-artistas=" + $scope.filtroArtista +
+                "&min=" + $scope.filtroFacilidade[0] +
+                "&max=" + $scope.filtroFacilidade[1]
+              )
+                .success(function (data, headers) {
+                  musicas = data;
+                })
+                .error(function () {
+                  console.log("Erro ao tentar pesquisar por acordes!");
+                })
+          }
+        })
+})();
+

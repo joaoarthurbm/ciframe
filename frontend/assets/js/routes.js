ed1bc02b8a5e3712576490b91b697063198bef3b
Jefferson Neves <jefferson.rpn@gmail.com>
2016-02-22 16:31:34 -0300

    Add initial pages

--- frontend/assets/js/routes.js
+++ frontend/assets/js/routes.js
@@ -4,6 +4,18 @@ angular.module('deciframeApp').config(function($routeProvider) {
       templateUrl: "templates/home.html",
       controller: "HomeController"
     })
+    .when('/poracordes', {
+      templateUrl: "templates/acordes.html",
+      controller: "AboutController"
+    })
+    .when('/pormusica', {
+      templateUrl: "templates/musica.html",
+      controller: "AboutController"
+    })
+    .when('/porsequencia', {
+      templateUrl: "templates/sequencia.html",
+      controller: "AboutController"
+    })
     .when('/sobre', {
       templateUrl: "templates/about.html",
       controller: "AboutController"

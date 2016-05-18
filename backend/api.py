be5ecd05b6f80eb9f02cd4c7a0f1ef2022136728
João Arthur <joaoarthurbm@gmail.com>
2016-05-18 20:22:11 -0300

    é dia 10. o que aconteceu? papai recebeu. papai recebeu.

--- backend/api.py
+++ backend/api.py
@@ -27,11 +27,13 @@ def init():
     f = open('../data/top/dataset_final.csv')
     f.readline()
     
-    musicas_dict = {}
     global generos
     global musicas
     global sequencias
+    global acordes
+    
     generos = set()
+    acordes = set()
     sequencias = {'BmGDA' : 0,
             'CGAmF' : 1,
             'EmG' : 2,
@@ -39,6 +41,7 @@ def init():
             'GmF' : 4, 
             'CC7FFm' : 5}
     
+    musicas_dict = {}
     for line in f:
         line = line.replace('"', '').replace('NA', '')[:-1]
 
@@ -53,6 +56,9 @@ def init():
         musica_obj = Musica(musica[ARTISTA_ID], musica[ARTISTA], musica[MUSICA_ID], musica[MUSICA],
                 musica[GENERO], int(musica[POPULARIDADE]), musica[SEQ_FAMOSA], musica[TOM], musica[CIFRA])
         musicas_dict[musica_obj.id_unico_musica] = musica_obj
+
+        for acorde in musica_obj.acordes:
+            acordes.add(acorde)
     
     # dicionário cujos valores são ordenados por popularidade
     musicas = OrderedDict(sorted(musicas_dict.items(), key=lambda x: x[1].popularidade, reverse = True))
@@ -62,7 +68,10 @@ def init():
     
     f.close()
     
-
+''' Busca por músicas que possuem no título ou no nome do artista o argumento passado por key.
+    params: key e generos (opcional). Caso generos não sejam definidos, a busca não irá filtrar por gênero.
+    exemplo 1: /search?key=no dia em que eu saí de casa
+    exemplo 2: /search?key=no dia em que eu saí de casa&generos=Rock,Samba '''
 @app.route('/search')
 def busca():
     generos_tag = request.args.get('generos', [])
@@ -96,13 +105,22 @@ def remover_combinantes(string):
     string = unicodedata.normalize('NFD', string)
     return u''.join(ch for ch in string if unicodedata.category(ch) != 'Mn')
 
+''' Retorna as músicas armazenadas no sistema (ordenados por popularidade). 
+    O serviço é paginado. Cada página tem tamanho 100, por default.
+    params: pagina. Caso não seja definida a página, o valor default é 1.
+    exemplo 1: /musica?pagina=2
+    exemplo 2: /musica'''
 @app.route('/musica')
 def get_musicas():
     return json.dumps([v.__dict__ for v in musicas.values()])
 
-@app.route('/genero')
+@app.route('/generos')
 def get_generos():
     return json.dumps(generos)
+
+@app.route('/acordes')
+def get_acordes():
+    return json.dumps(list(acordes))
 
 @app.route('/musica/<m_id>/')
 def get_musica(m_id):

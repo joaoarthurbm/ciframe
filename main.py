f8d0d1612afc266c3dbafa680b65d078321a028a
Jefferson Neves <jefferson.rpn@gmail.com>
2016-10-05 10:02:44 -0300

    Vai pra lá, vai pra lá....

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def get_similares(acordes, generos_key, id_musica = None):
                         'intersecao' : list(inter)
                 }
 
-            similares.append(similar)
+                similares.append(similar)
 
     # ordenados por menor diferença, maior interseção e maior popularidade.
     return sorted(similares, key=lambda x: (len(x['diferenca']), -len(x['intersecao'])))
